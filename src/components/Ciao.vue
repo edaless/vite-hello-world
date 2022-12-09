c442809779cc264485a35d4118b2eaf768e292d1
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-12-09 19:06:14 +0100

    workin on component

---
+++ src/components/Ciao.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "Ciao",
+    data() {
+        return {
+            saluto: "Ciao a tutti dal componente!"
+        }
+    }
+}
+</script>
+
+<template>
+
+    <h1>Ciaoooo</h1>
+
+</template>
+
+<style scoped>
+h1 {
+    color: rgb(255, 0, 0);
+}
+</style>
